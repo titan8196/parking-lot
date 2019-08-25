a188e43c34646a95302d8b5735490fc60b258f27
himanshukgpt <gupta.himanshu8196@gmail.com>
2019-08-25 10:17:23 +0530

    Add conditions to avoid null Pointers

--- ParkingLot/src/main/java/parkinglot/ParkingLotMain.java
+++ ParkingLot/src/main/java/parkinglot/ParkingLotMain.java
@@ -42,28 +42,53 @@ public class ParkingLotMain {
 		splitCommand = command.split(" ");
 		switch(splitCommand[0]){
 			case CREATE_PARKING_LOT:
-				parkingLot = new ParkingLot(Integer.parseInt(splitCommand[1]));
+				if(splitCommand.length!=2){
+					System.out.println("Invalid command");
+				}else{
+					parkingLot = new ParkingLot(Integer.parseInt(splitCommand[1]));
+				}
 				break;
 			case PARK_VEHICLE:
-				String registrationNum = splitCommand[1];
-				String color = splitCommand[2];
-				Vehicle vehicle = new Car(registrationNum,color);
-				parkingLot.parkVehicle(vehicle);
+				if(splitCommand.length!=3){
+					System.out.println("Invalid command");
+				}
+				else{
+					String registrationNum = splitCommand[1];
+					String color = splitCommand[2];
+					Vehicle vehicle = new Car(registrationNum,color);
+					parkingLot.parkVehicle(vehicle);
+				}
 				break;
 			case LEAVE_SPOT:
-				parkingLot.leave(Integer.parseInt(splitCommand[1]));
+				if(splitCommand.length!=2){
+					System.out.println("Invalid command");
+				}else{
+					parkingLot.leave(Integer.parseInt(splitCommand[1]));
+				}
 				break;
 			case PRINT_PARKING_LOT:
 				parkingLot.status();
 				break;
 			case PRINT_CAR_NUM_FOR_COLOR:
-				parkingLot.getAllCarsWithColor(splitCommand[1], TYPE_REGISTRATION_NUMBER);
+				if(splitCommand.length!=2){
+					System.out.println("Invalid command");
+				}else{
+					parkingLot.getAllCarsWithColor(splitCommand[1], TYPE_REGISTRATION_NUMBER);
+				}
 				break;
 			case PRINT_SLOT_NUM_FOR_COLOR:
-				parkingLot.getAllCarsWithColor(splitCommand[1], TYPE_SLOT_NUMBER);
+				if(splitCommand.length!=2){
+					System.out.println("Invalid command");
+				}else{
+					parkingLot.getAllCarsWithColor(splitCommand[1], TYPE_SLOT_NUMBER);
+				}
 				break;
 			case PRINT_SLOT_NUM_FOR_CAR_NUM:
-				parkingLot.getSlotNumForRegistrationNum(splitCommand[1]);
+				if(splitCommand.length!=2){
+					System.out.println("Invalid command");
+				}else{
+					parkingLot.getSlotNumForRegistrationNum(splitCommand[1]);
+				}
 				break;
 			case EXIT:
 				exitFlag=1;
